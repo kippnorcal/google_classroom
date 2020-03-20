6fec753a21a30d6f6d5f23112f49dbc529d3291d
dhess <dan.hess@kippbayarea.org>
2020-03-20 11:24:14 -0700

    PROJ-1785 Combine coursework into main

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ import pandas as pd
 from sqlsorcery import MSSQL
 
 
-
 def build_service():
     # If modifying these scopes, delete the file token.pickle.
-SCOPES = [
-    "https://www.googleapis.com/auth/classroom.student-submissions.students.readonly",
-    "https://www.googleapis.com/auth/classroom.courses",
-    "https://www.googleapis.com/auth/classroom.rosters",
-]
+    SCOPES = [
+        "https://www.googleapis.com/auth/classroom.student-submissions.students.readonly",
+        "https://www.googleapis.com/auth/classroom.courses",
+        "https://www.googleapis.com/auth/classroom.rosters",
+    ]
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -39,6 +38,7 @@ SCOPES = [
 
     service = build("classroom", "v1", credentials=creds)
     return service
+
 
 def get_courses(service):
     # Get all paginated courses
@@ -71,29 +71,90 @@ def get_teachers(service, course_ids):
         results = service.courses().teachers().list(courseId=course_id).execute()
         teachers = results.get("teachers", [])
         all_teachers.extend(teachers)
-    return teachers
+    return all_teachers
+
+
+def get_student_submissions(service, course_ids):
+    # For the given courses, get the list of student coursework submissions.
+    all_submissions = []
+    for course_id in course_ids:
+        print(f"getting student submissions for {course_id}")
+        results = (
+            service.courses()
+            .courseWork()
+            .studentSubmissions()
+            .list(courseId=course_id, courseWorkId="-")
+            .execute()
+        )
+        student_submissions = results.get("studentSubmissions", [])
+        all_submissions.extend(student_submissions)
+    return all_submissions
+
+
+def parse_statehistory(record, parsed):
+    submission_history = record.get("submissionHistory")
+    if submission_history:
+        for submission in submission_history:
+            state_history = submission.get("stateHistory")
+            if state_history:
+                state = state_history.get("state")
+                if state == "CREATED":
+                    parsed["createdTime"] = state_history.get("stateTimestamp")
+                elif state == "TURNED_IN":
+                    parsed["turnedInTimestamp"] = state_history.get("stateTimestamp")
+                elif state == "RETURNED":
+                    parsed["returnedTimestamp"] = state_history.get("stateTimestamp")
+
+
+def parse_gradehistory(record, parsed):
+    submission_history = record.get("submissionHistory")
+    if submission_history:
+        for submission in submission_history:
+            grade_history = submission.get("gradeHistory")
+            if grade_history:
+                grade_change_type = grade_history.get("gradeChangeType")
+                if grade_change_type == "DRAFT_GRADE_POINTS_EARNED_CHANGE":
+                    parsed["draftMaxPoints"] = grade_history.get("maxPoints")
+                    parsed["draftGradeTimestamp"] = grade_history.get("gradeTimestamp")
+                    parsed["draftGraderId"] = grade_history.get("actorUserId")
+                elif grade_change_type == "ASSIGNED_GRADE_POINTS_EARNED_CHANGE":
+                    parsed["assignedMaxPoints"] = grade_history.get("maxPoints")
+                    parsed["assignedGradeTimestamp"] = grade_history.get(
+                        "gradeTimestamp"
+                    )
+                    parsed["assignedGraderId"] = grade_history.get("actorUserId")
+
+
+def parse_coursework(coursework):
+    records = []
+    for record in coursework:
+        parsed = {
+            "courseId": record.get("courseId"),
+            "courseWorkId": record.get("courseWorkId"),
+            "id": record.get("id"),
+            "userId": record.get("userId"),
+            "creationTime": record.get("creationTime"),
+            "updateTime": record.get("updateTime"),
+            "state": record.get("state"),
+            "draftGrade": record.get("draftGrade"),
+            "assignedGrade": record.get("assignedGrade"),
+            "courseWorkType": record.get("courseWorkType"),
+        }
+        parse_statehistory(record, parsed)
+        parse_gradehistory(record, parsed)
+        records.append(parsed)
+    return records
 
 
 def main():
     service = build_service()
 
-    # coursework = (
-    #     service.courses()
-    #     .courseWork()
-    #     .studentSubmissions()
-    #     .list(courseId="41136730708", courseWorkId="-")
-    #     .execute()
-    # )
-
-    # with open("coursework.json", "w", encoding="utf-8") as f:
-    #     json.dump(coursework, f, ensure_ascii=False, indent=4)
-
     sql = MSSQL()
     # Get courses
     courses = get_courses(service)
     courses = json_normalize(courses)
-    # courses = courses.astype(str)
-    # sql.insert_into("GoogleClassroom_Courses", courses) # this is erroring on insertion
+    courses = courses.astype(str)
+    # sql.insert_into("GoogleClassroom_Courses", courses)  # this is erroring on insertion
     course_ids = courses.id.unique()
 
     # Get students and insert into database
@@ -107,6 +168,14 @@ def main():
     teachers = json_normalize(teachers)
     teachers = teachers.astype(str)
     sql.insert_into("GoogleClassroom_Teachers", teachers)
+
+    # Get student coursework submissions
+    student_submissions = get_student_submissions(service, course_ids)
+    student_submissions = parse_coursework(student_submissions)
+    student_submissions = pd.DataFrame(student_submissions)
+    sql.insert_into(
+        "GoogleClassroom_Coursework", student_submissions, if_exists="replace"
+    )
 
 
 if __name__ == "__main__":
