f0aae98f3a1d2dd5ac0383c3a87958376dbe4666
dhess <dan.hess@kippbayarea.org>
2020-03-27 10:36:56 -0700

    Add timer module for timing function elapsed time

---
+++ timer.py
@@ -0,0 +1,27 @@
+import time
+import logging
+
+
+def _timer_message(func, elapsed):
+    """Private function for formatting log message so the correct
+    unit is displayed based on the elapsed seconds"""
+    unit = "seconds"
+    if elapsed >= 60:
+        elapsed = round(elapsed / 60, 2)
+        unit = "minutes"
+
+    return f"{func.__name__} completed in {elapsed} {unit}."
+
+
+def elapsed(func):
+    """Decorator function used to compute and report the time a
+    function takes to complete. Add to a function as: @elapsed"""
+
+    def wrapper(*args):
+        start = time.time()
+        func(*args)
+        end = time.time()
+        elapsed = round(end - start, 2)
+        logging.info(_timer_message(func, elapsed))
+
+    return wrapper
