6b5d30b1d554d3fa88bd5f71e08dbd0d646c8ee9
Zach Kagin <zkagin@gmail.com>
2020-09-30 12:30:32 -0700

    Allow test results to actually trigger github workflow pass/fail (#107)
    Fixes #106
    Fixes an issue where the Github Action was not properly failing when tests failed. This propagates the test failure to the action itself.
    
    Note that the result has to be stored after the first docker-compose call, because otherwise $? will be overwritten by the results of the second call (which should always be successful).

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from app
+TEST_RESULT=$?
 docker-compose -f docker-compose.test.yml down --volumes
+exit $TEST_RESULT
