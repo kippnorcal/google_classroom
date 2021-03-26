fa31b16b21b35937688dabe58e56f3a4584e020e
Zach Kagin <zkagin@gmail.com>
2021-03-25 23:04:19 -0400

    fix black errors

--- google_classroom/endpoints/base.py
+++ google_classroom/endpoints/base.py
@@ -304,7 +304,7 @@ class EndPoint:
             both:       A dataframe containing data found in both df1 and df2.
         """
         merged = pd.merge(
-            df1, df2, left_on=left_on, right_on=right_on, how="outer", indicator=True,
+            df1, df2, left_on=left_on, right_on=right_on, how="outer", indicator=True
         )
         left_only = merged[merged["_merge"] == "left_only"].reset_index(drop=True)
         right_only = merged[merged["_merge"] == "right_only"].reset_index(drop=True)
